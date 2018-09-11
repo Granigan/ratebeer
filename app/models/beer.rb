b31eae3bb073b4d4a7c7ffb84365c2719836d421
Tero Tapio <teromarkustapio@gmail.com>
2018-09-11 17:31:33 +0300

    6/16

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,8 +2,12 @@ class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
 
-    def average_rating
+    def average_rating_old
         ratings.average('score')
+    end
+
+    def average_rating
+        ratings.map{|r| r.score}.reduce(:+) / ratings.count
     end
 
 end
