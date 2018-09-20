54fe0da029d83e42cde172deaf3f19f59e0ab50c
Tero Tapio <teromarkustapio@gmail.com>
2018-09-20 13:20:12 +0300

    validates ratings

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by username: params[:username]
     if user.nil?
-      redirect_to signin_path, notice: ("User #{params[:username]} does not exist!")
+      redirect_to signin_path, notice: "User #{params[:username]} does not exist!"
     else
       session[:user_id] = user.id if user
       redirect_to user, notice: "Welcome back!"
