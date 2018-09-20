34b892180962ddaf2b43679cd84f29a6bd55f99e
Tero Tapio <teromarkustapio@gmail.com>
2018-09-20 19:34:47 +0300

    w3_10/16

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
 
   has_many :ratings
   has_many :beers, through: :ratings
+  has_many :memberships, dependent: :destroy
+  has_many :beerclubs, through: :memberships
   
 end
