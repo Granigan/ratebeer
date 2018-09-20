9702b3eeadf4bdbb158ff3c6873f0b4682a95ced
Tero Tapio <teromarkustapio@gmail.com>
2018-09-20 22:09:48 +0300

    task 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,10 +54,13 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy if @user == current_user
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if @user == current_user
+      session[:user_id] = nil
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
