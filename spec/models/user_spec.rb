675c7b894cc6d51c191131fd9f1acbda673b507c
Tero Tapio <teromarkustapio@gmail.com>
2018-09-25 22:20:01 +0300

    w4t1, adds first tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has the username set correctly" do
+    user = User.new username:"Pekka"
+
+    expect(user.username).to eq("Pekka")
+  end
+
+  it "is not saved without a password" do
+    user = User.create username:"Pekka"
+
+    expect(user.valid?).to be(false)
+    expect(User.count).to eq(0)
+  end
+
+  it "is not saved with a too short password" do
+    user = User.create username:"Pekka", password:"Se1",
+      password_confirmation:"Se1"
+
+    expect(user.valid?).to be(false)
+    expect(User.count).to eq(0)
+  end
+
+  it "is not saved with a letters-only password" do
+    user = User.create username:"Pekka", password:"secrets", 
+      password_confirmation:"secrets"
+
+    expect(user.valid?).to be(false)
+    expect(User.count).to eq(0)
+  end
+
+  describe "with a proper password" do
+    let(:user) {User.create username:"Pekka", password:"Secret1", 
+      password_confirmation:"Secret1"}
+    let(:test_brewery) {Brewery.new name:"test", year:2000}
+    let(:test_beer) {Beer.create name:"testbeer", style:"teststyle", 
+      brewery:test_brewery}
+  
+    it "is saved" do
+      expect(user).to be_valid
+      expect(User.count).to eq(1)
+    end
+
+    it "and with two ratings, has the correct average rating" do
+      rating = Rating.new score:10, beer: test_beer
+      rating2 = Rating.new score:20, beer: test_beer
+
+      user.ratings << rating
+      user.ratings << rating2
+
+      expect(user.ratings.count).to eq(2)
+      expect(user.average_rating).to eq(15)
+    end
+  end
 end
+
