22f38048df3f2e62eef4f18fe7ec3896c905dbd0
Tero Tapio <teromarkustapio@gmail.com>
2018-10-04 17:21:55 +0300

    w5t3 - parsing tests

--- Gemfile
+++ Gemfile
@@ -71,6 +71,7 @@ group :test do
   gem 'chromedriver-helper'
   gem 'launchy'
   gem 'simplecov', require: false
+  gem 'webmock'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'simplecov'
 SimpleCov.start('rails')
 require 'spec_helper'
 require 'helpers'
+require 'webmock/rspec'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
