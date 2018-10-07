7e26dde09f95b76a266f2f0add688b4bf5c6b86c
Tero Tapio <teromarkustapio@gmail.com>
2018-10-07 18:54:39 +0300

    migrations link beers to styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_153801) do
+ActiveRecord::Schema.define(version: 2018_10_07_155217) do
 
   create_table "beerclubs", force: :cascade do |t|
     t.string "name"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 2018_10_07_153801) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
-    t.string "style"
+    t.string "old_style"
     t.integer "brewery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "style_id"
   end
 
   create_table "breweries", force: :cascade do |t|
