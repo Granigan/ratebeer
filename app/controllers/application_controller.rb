9ba5cdd8becd49feaad51f1b8ac703757e490716
Tero Tapio <teromarkustapio@gmail.com>
2018-10-13 22:39:15 +0300

    w6t10 - beer,brewery,club deletion by admin only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
   def ensure_that_signed_in
     redirect_to signin_path, notice: 'You need to be signed in to use this feature.' if current_user.nil?
   end
+
+  def admin_user
+    User.find(session[:user_id]).admin
+  end
+
+  def ensure_that_user_is_admin
+    redirect_to current_user, notice: 'You are not an admin.' unless admin_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_103322) do
+ActiveRecord::Schema.define(version: 2018_10_13_185659) do
 
   create_table "beerclubs", force: :cascade do |t|
     t.string "name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_10_12_103322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin"
   end
 
 end
